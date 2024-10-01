8ccf8a6a479ef4e701129d2e8247605a8c352d41
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-10-01 18:12:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+let btn = document.getElementById("btn")
+
+btn.addEventListener("click", delayText)
+
+async function delayText(delay, text){
+	let delay = document.getElementById("delay").value
+let text = document.getElementById("text").value
+	
+       await new Promise((resolve, reject) => {
+               setTimeout( resolve ,delay)
+       })
+
+	output.textContent = text
+}
